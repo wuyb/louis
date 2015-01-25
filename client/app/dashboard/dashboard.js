4543cac115b73b1bec4972fdcc6460e6e9257976
Yanbo Wu <yanbo.wu@gmail.com>
2015-01-26 01:53:17 +0800

    initial commit

---
+++ client/app/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/dashboard', {
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+      });
+  });
---
+++ client/app/main/main.controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('app')
+  .controller('MainCtrl', function ($scope, $http, socket, Auth) {
+    $scope.awesomeThings = [];
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+  });
